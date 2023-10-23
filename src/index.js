056492928ca0c3cb2123e781dff0dedf896ce19a
Milly <lauraa9lemos@gamail.com>
2023-10-23 17:43:59 -0300

    Commit de teste

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 require('dotenv').config()
 const express = require('express')
 
+const carros = ['S10','Hilux','Ford Ranger']
+
 const app = express()
 
 app.use(express.json())
 
 app.get('/', async (req, res) => {
-    return res.json('Api está ok')
+    return res.json(carros)
 })
 
 const port = process.env.PORT  || 3000
